7a3e9773a5acc2e90ac66b7b85e26418e9e5f8cd
William Judd Ebert <juddebert@yahoo.com>
2026-03-28 18:51:22 -0400

    build_index function in indexer.py, honestly i'm a little hazy on some of the metadata/faiss stuff but I'll prolly understand it as i use it more

--- backend/ingestion/indexer.py
+++ backend/ingestion/indexer.py
@@ -2,3 +2,35 @@
 Judd Ebert 3/15/2026
 Go from vectors to searchable index
 """
+
+FLOATS_PER_CHUNK=3072
+STORAGE_DIR = "backend/storage"
+
+import faiss
+import hashlib
+import numpy as np
+import json
+import os
+def build_index(chunks: list[dict], github_url:str):
+    chunks_array = np.zeros((len(chunks), FLOATS_PER_CHUNK), dtype=np.float32)
+    for i, chunk in enumerate(chunks):
+        chunks_array[i] = chunk["embedding"]
+    
+    faiss.normalize_L2(chunks_array) #Normalizes all arrays for cosine similarity
+
+    index = faiss.IndexFlatIP(FLOATS_PER_CHUNK) #Index for searching
+    index.add(chunks_array)
+
+    repo_id = hashlib.md5(github_url.encode()).hexdigest()[:12]
+    os.makedirs(STORAGE_DIR, exist_ok=True)
+
+    faiss.write_index(index, f"{STORAGE_DIR}/{repo_id}.index")
+
+    metadata = [
+        {k: v for k, v in chunk.items() if k!= "embedding"}
+        for chunk in chunks
+    ]
+    with open(f"{STORAGE_DIR}/{repo_id}.json", "w") as f:
+        json.dump(metadata, f)
+
+    return repo_id
